d5ffc65019e3dce18a6527cc8f00a87b23ff1657
Eduardo Quiroga <ed.quiroga.2103@gmail.com>
2025-09-09 01:22:15 -0600

    Fixed navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -2,24 +2,24 @@ import { label } from "framer-motion/client";
 import Link from "next/link";
 import React from "react";
 
-const routes = [
-  {
-    label: "Services",
-    path: "/services",
-  },
-  {
-    label: "Portfolio",
-    path: "/portfolio",
-  },
-  {
-    label: "About",
-    path: "/about",
-  },
-  {
-    label: "Contact",
-    path: "/contact",
-  },
-];
+// const routes = [
+//   // {
+//   //   label: "Services",
+//   //   path: "/services",
+//   // },
+//   // {
+//   //   label: "Portfolio",
+//   //   path: "/portfolio",
+//   // },
+//   // {
+//   //   label: "About",
+//   //   path: "/about",
+//   // },
+//   // {
+//   //   label: "Contact",
+//   //   path: "/contact",
+//   // },
+// ];
 
 export default function index() {
   return (
@@ -30,11 +30,11 @@ export default function index() {
         </Link>
       </div>
       <div className="links">
-        {routes.map((route) => (
+        {/* {routes.map((route) => (
           <Link className="link" key={route.path} href={route.path}>
             {route.label}
           </Link>
-        ))}
+        ))} */}
       </div>
     </div>
   );
